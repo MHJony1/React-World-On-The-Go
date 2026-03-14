083856faff6417c1fcec09636525031e4f7ca842
jonym7293-alt <jonym7293@gmail.com>
2026-03-15 01:45:34 +0600

    working on Countries setup project by React

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
 
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+ .then(res => res.json())
+
 function App() {
   return (
-    <>
+    <> 
       
-      
-        
-      <h1>React World On The Go...</h1>
-      <Countries></Countries>
-         
-       
-       
-      
-
-      
-
-      
+      <Suspense fallback = {<p>Data Loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+          
      
     </>
   )
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './Country.css';
+
+const Country = ({country}) => {
+  const[visited, setVisited] = useState(false);
+ 
+  // console.log(country);
+
+  const handleVisited = () => {
+    // system-1
+    // if(visited){
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // system-2
+    // setVisited(visited ? false : true);
+    // system-3
+    setVisited(!visited);
+
+  }
+  return (
+    <div className={`country ${visited && 'country-visited'}`}>
+      <img src={country?.flags?.flags?.png} alt={country?.flags?.flags?.alt} />
+      <h3>Name: {country?.name?.common}</h3>
+      <p>Capital: {country?.capital?.capital}</p>
+      <p>Region: {country?.region?.region}</p>
+      <p>Language: {country?.languages?.languages ? Object.values(country.languages.languages).join(', ') : 'N/A'}</p>
+      <p>Area: {country?.area?.area} {country?.area?.area > 300000 ? "(Big Country)" : "(Small Country)"}</p> 
+      <p>Currency: {country?.currencies?.currencies ? Object.values(country.currencies.currencies).map(c => `${c.name} (${c.symbol})`).join(', ') : 'N/A'}</p>
+      <p>Population: {country?.population?.population}</p>
+      <button onClick={handleVisited} className='btn'>{visited ? "Visited" : "Not Visited"}</button>
+    </div>
+  );
+};
+
+export default Country;
